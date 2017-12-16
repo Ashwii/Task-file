dd9834f9573d327109c168b70d6b050785b4793f
Ashwii <ashwinianandhraj@gmail.com>
2017-12-16 07:19:36 +0530

    First commit

---
+++ two way-binding.js
@@ -0,0 +1,18 @@
+	function myfunc(){
+		var text= document.getElementById("one").value;
+		document.getElementById("two").innerHTML = "Hello"+" "+text+"!!";
+		document.getElementById("three").value=text;
+		return true;
+	}
+	function time(){
+			setInterval(myfunc,50);
+	}
+	function myfunct(){
+			var text1=document.getElementById("three").value;
+			document.getElementById("two").innerHTML="Hello"+" "+text1+"!!";
+			document.getElementById("one").value=text1;
+			return true;
+	}
+	function time1(){
+			setInterval(myfunct,50);
+	}
