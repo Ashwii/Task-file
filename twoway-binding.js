fbb3907d161b6cfc4f9d59025cc87ed6eee7e2db
Ashwii <ashwinianandhraj@gmail.com>
2017-12-19 10:31:23 +0530

    commiting

--- twoway-binding.js
+++ twoway-binding.js
@@ -1,4 +1,4 @@
-	function myfunc(){
+    function myfunc(){
 		var text= document.getElementById("one").value;
 		document.getElementById("two").innerHTML = "Hello"+" "+text+"!!";
 		document.getElementById("three").value=text;
@@ -9,10 +9,11 @@
 	}
 	function myfunct(){
 			var text1=document.getElementById("three").value;
-			document.getElementById("two").innerHTML="Hello"+" "+text1+"!!";
-			document.getElementById("one").value=text1;
+			document.getElementById("two").innerHTML = "Hello"+" "+text1+"!!";
+			document.getElementById("one").value = text1;
 			return true;
 	}
 	function time1(){
 			setInterval(myfunct,50);
 	}
+	
